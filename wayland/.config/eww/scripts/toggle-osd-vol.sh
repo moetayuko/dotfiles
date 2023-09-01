875f8405cd3d7af29bcc92705149e46d3d64151b
MoetaYuko <loli@yuko.moe>
2023-09-01 21:25:32 +0800

    Update

---
+++ wayland/.config/eww/scripts/toggle-osd-vol.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+cd ~/.config/eww
+
+if [[ "$1" == "--open" ]]; then
+	eww update osd_vol=true
+elif [[ "$1" == "--close" ]]; then
+	eww update osd_vol=false
+else
+	eww update osd_vol=true
+fi
