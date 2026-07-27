5fcd5ad558e1034d20f126b763f5bde4d72a20dc
Moeta Yuko <loli@yuko.moe>
2026-07-27 19:00:18 +0100

    update

--- hyprland/.config/hypr/custom/keybinds.lua
+++ hyprland/.config/hypr/custom/keybinds.lua
@@ -3,7 +3,3 @@ hl.bind(
 	hl.dsp.exec_cmd("pkill rofi || ~/.config/rofi/launchers/type-6/launcher.sh"),
 	{ description = "Toggle rofi launcher" }
 )
-
-hl.unbind("SUPER + Tab")
-hl.bind("SUPER + Tab", hl.dsp.focus({ workspace = "previous_per_monitor'" }))
-hl.config({ binds = { workspace_back_and_forth = true } })
